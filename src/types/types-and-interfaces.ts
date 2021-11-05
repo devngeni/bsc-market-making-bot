ddeef478fbec68c2f30f723f9a227682c2c118c5
Omambia Dauglous <omambiadauglous@gmail.com>
2021-11-05 16:00:58 +0300

    Adding types for configurations

---
+++ src/types/types-and-interfaces.ts
@@ -0,0 +1,16 @@
+/**
+ * Base Config types
+ * This types define what are all the required information for use to define in inthe
+ * config/prod.ts file
+ * *******************************
+ * ****PLEASE DON'T CHANGE THIS IF YOU DON'T KNOW WHAT YOU ARE DOING****
+ * *******************************
+ */
+export interface Config {
+  WALLETS: Array<{ PRIVATE_KEY: string; ADDRESS: string }>;
+  PRICEIMPACT: { SELLING: number; BUYING: number };
+  EXECUTION_TIME: Array<number>;
+  BLOXROUTE: {
+    ENDPOINT: string;
+  };
+}
